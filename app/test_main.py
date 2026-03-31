ca8075e8b15250ba1a1c330d7a288a73dfd8c81e
Vijay Chand <vijaychand6979@googlemail.com>
2026-03-31 12:49:22 +0100

    corrected test cases

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,19 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_less_than_15_years() -> None:
-    assert get_human_age(14, 14) == [0, 0], (
-        "less than 15 years does not equal [0, 0]"
-    )
-
-
-def test_15_years() -> None:
-    assert get_human_age(15, 15) == [1, 1], (
-        "first 15 years does not equal [1, 1]"
-    )
-
-
-def test_24_years() -> None:
-    assert get_human_age(24, 24) == [2, 2], (
-        "24 years does not equal [2, 2]"
-    )
-
-
-def test_28_years() -> None:
-    assert get_human_age(28, 28) == [3, 2], (
-        "28 years does not equal [3, 2]"
-    )
-
-
-def test_100_years() -> None:
-    assert get_human_age(100, 100) == [21, 17], (
-        "100 years does not equal [21, 17]"
-    )
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_eval(cat_age: int, dog_age: int, expected: list[int]) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
